b719f02cd91590bcf33016857520db3079af8ddc
PradnyaS <93059012+Pradny2904@users.noreply.github.com>
2021-11-27 21:05:03 +0530

    Add files via upload

---
+++ fetching nos.py
@@ -0,0 +1,13 @@
+
+s=('Hello, this is police and if any help contact on this no 0123456789')
+s.split()
+for i in s:
+    if i.isnumeric()==True:
+        print(i, end='')
+'''
+s= ('My name is guru and my roll no is 12')
+s.split()
+for i in s:
+    if i==guru:
+        print(i)
+'''
